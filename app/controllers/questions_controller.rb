0293446dcf04e16dca9416902da0f10dbdc9c620
jeanne BAILHACHE <jeanne.bailhache@gmail.com>
2015-06-03 16:38:17 +0200

    fix button for js

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,9 +2,9 @@ class QuestionsController < ApplicationController
   def index
     @questions_answered = Question.joins("LEFT JOIN question_voters ON question_voters.question_id = questions.id").group("questions.id").where(:room_id => params[:room_id], :answered => true).order("questions.answered, sum(question_voters.vote)").reverse
     @questions = Question.joins("LEFT JOIN question_voters ON question_voters.question_id = questions.id").group("questions.id").where(:room_id => params[:room_id]).order("questions.answered, sum(question_voters.vote)").reverse
-	if user_signed_in?
-		@user_is_editor = RoomsUser.where(user_id: current_user.id, room_id: params[:room_id], editor: true).first
-	end
+		if user_signed_in?
+				@user_is_editor = RoomsUser.where(user_id: current_user.id, room_id: params[:room_id], editor: true).first
+		end
   end
 
   def new
@@ -36,8 +36,8 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
     if user_signed_in? and (current_user.admin? or RoomsUser.where(:user_id => current_user.id, :room_id => params[:room_id]).first.editor? rescue nil)
       @question.update_attributes(:answered => true)
-	end
-	render :nothing => true
+		end
+		redirect_to :back
   end
 
   def destroy
@@ -48,8 +48,8 @@ class QuestionsController < ApplicationController
     redirect_to :back
   end
 
-  def answer
-
+  def show
+		redirect_to :back
   end
 
   private
